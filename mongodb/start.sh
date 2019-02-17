c199a061b57e39ef830689a0912911a3acc7f42d
Phillip Lakis <p.lakis@icloud.com>
2019-02-18 08:26:55 +1100

    Update start.sh

--- mongodb/start.sh
+++ mongodb/start.sh
@@ -4,24 +4,24 @@ echo ""
 echo "Setting up the host."
 echo ""
 echo "********************************************************************************"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
+apt-get update
 echo "********************************************************************************"
 echo ""
 echo "MongoDB"
 echo ""
 echo "********************************************************************************"
 sleep 5
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
 echo "********************************************************************************"
 echo ""
 echo "Firewall"
 echo ""
 echo "********************************************************************************"
 sleep 5
-sudo ufw allow from your_other_server_ip/32 to any port 27017
-sudo ufw enable
-sudo ufw status
+ufw allow from your_other_server_ip/32 to any port 27017
+ufw enable
+ufw status
