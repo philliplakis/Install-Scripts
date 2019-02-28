534e0ae3d5efd09648ae735e9dd730e67ad49cd7
Phillip Lakis <p.lakis@icloud.com>
2019-03-01 09:51:51 +1100

    Create start.sh

---
+++ systemsetup/ubuntu/start.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Ubuntu Minimal 16.04
+
+echo "********************************************************************************"
+echo "Update / NVIDIA / VNC"
+echo "********************************************************************************"
+sleep 2
+
+wget https://raw.githubusercontent.com/philliplakis/gpu-vnc/master/start.sh
+bash start.sh -y
+
+echo "********************************************************************************"
+echo "Cloud-Init Install"
+echo "********************************************************************************"
+sleep 2
+
+apt-get install cloud-init -y
+
+echo "********************************************************************************"
+echo "Installing NodeJS."
+echo "********************************************************************************"
+sleep 2
+
+apt-get install nodejs -y
+
+echo "********************************************************************************"
+echo "Installing NVSMI."
+echo "********************************************************************************"
+sleep 2
+
+cd /etc
+git clone https://github.com/philliplakis/nvsmi.git
+cd nvsmi
+npm install
+npm install forever -g
+forever start server.js
+
+echo "********************************************************************************"
+echo "Setup welcome message"
+echo "********************************************************************************"
+
+mkdir /etc/ssh/banner/
+cd /etc/ssh/banner/
+wget https://raw.githubusercontent.com/philliplakis/cheatsheets/master/systemsetup/welcome
+cd /etc/ssh/
+echo "Banner /etc/ssh/banner/welcome" >> ./sshd_config
+cd
+
+echo "********************************************************************************"
+echo "Rebooting"
+echo "********************************************************************************"
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+reboot
