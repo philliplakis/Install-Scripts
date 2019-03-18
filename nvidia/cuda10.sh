cac3955b0ca12473fc73b8c7b7d7361b011c51df
Phillip Lakis <p.lakis@icloud.com>
2019-03-19 09:07:38 +1100

    Create cuda10.sh

---
+++ nvidia/cuda10.sh
@@ -0,0 +1,9 @@
+wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
+
+chmod +x cuda_10.1.105_418.39_linux.run
+
+sudo ./cuda_10.1.105_418.39_linux.run --silent --toolkit --samples
+
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/418.43/NVIDIA-Linux-x86_64-418.43.run
+
+sudo bash NVIDIA-Linux-x86_64-418.43.run
